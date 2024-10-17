a56c2f00ea4af972ddbffae94305f267afe8cd81
shersheryar <p218027@pwr.nu.edu.pk>
2024-10-18 04:44:30 +0500

    signup page validation

--- backend/ai_blog_app/blog_generator/views.py
+++ backend/ai_blog_app/blog_generator/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
 
@@ -18,11 +20,36 @@ def user_login(request):
 
 
 def user_signup(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        repeatpassword = request.POST.get('repeatpassword')
+
+        if password == repeatpassword:
+            if User.objects.filter(username=username).exists():
+                error_message = 'Username already exists'
+                return render(request, 'signup.html', {'error_message': error_message})
+            elif User.objects.filter(email=email).exists():
+                error_message = 'Email already exists'
+                return render(request, 'signup.html', {'error_message': error_message})
+            else:
+                user = User.objects.create_user(
+                    username=username, email=email, password=password)
+                user.save()
+                #  This is the login function that logs in the user after signing up successfully
+                login(request, user)
+                #  This is the redirect function that redirects the user to the index page after signing up successfully
+                return redirect('/')
+        else:
+            error_message = 'Passwords do not match'
+            return render(request, 'signup.html', {'error_message': error_message})
+        return render(request, 'login.html')
     return render(request, 'signup.html')
 
 #  This is the view for the logout page
 
 
 def user_logout(request):
-     
+
     return render(request, 'logout.html')
